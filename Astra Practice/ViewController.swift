d2fe00a13d45cf9353680c6c80a1ce5aa942484c
luthfyra <luthfyrahmani04@gmail.com>
2023-08-02 14:00:39 +0700

    Initial Commit

---
+++ Astra Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Astra Practice
+//
+//  Created by PRO M1 2020 8/256 on 02/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
