bc6492af4ae2506732664357039f145dbe3d9908
luthfyra <luthfyrahmani04@gmail.com>
2023-08-04 16:44:51 +0700

    Initial Commit

---
+++ Scene/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Scene
+//
+//  Created by PRO M1 2020 8/256 on 04/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scene/SceneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SceneApp.swift
+//  Scene
+//
+//  Created by PRO M1 2020 8/256 on 04/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SceneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
