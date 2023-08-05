26179e8cbb580ff4b95235b6d6f3ad352bf789c5
luthfyra <luthfyrahmani04@gmail.com>
2023-08-05 19:07:01 +0700

    Initial Commit

---
+++ CRUD Operation/CRUD_OperationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUD_OperationApp.swift
+//  CRUD Operation
+//
+//  Created by PRO M1 2020 8/256 on 05/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CRUD_OperationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
