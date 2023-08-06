5cd3163ff7939e2ea6357eb76a0ab89e51abfc3f
luthfyra <luthfyrahmani04@gmail.com>
2023-08-06 23:56:24 +0700

    Initial Commit

---
+++ MyNotes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyNotes
+//
+//  Created by PRO M1 2020 8/256 on 06/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyNotes/MyNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNotesApp.swift
+//  MyNotes
+//
+//  Created by PRO M1 2020 8/256 on 06/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MyNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
