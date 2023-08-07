0299462f47f1aba3230fcb1b0b513e2986d7f4fd
luthfyra <luthfyrahmani04@gmail.com>
2023-08-07 09:30:47 +0700

    Initial Commit

---
+++ Auto2000/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto2000
+//
+//  Created by PRO M1 2020 8/256 on 07/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
