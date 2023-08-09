3a81ae09570334b8092b289412e2cee6a9def815
luthfyra <luthfyrahmani04@gmail.com>
2023-08-09 09:00:23 +0700

    Initial Commit

---
+++ Auto2000Digiroom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto2000Digiroom
+//
+//  Created by PRO M1 2020 8/256 on 09/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
