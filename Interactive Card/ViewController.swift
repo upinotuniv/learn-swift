5b708ba525fd5a4a69fb094e6aaabfc5a27e3dea
LuthfyRa.1204 <Luthfy.Rahmani@mii.co.id>
2023-08-17 15:04:49 +0700

    bottom sheet

--- Interactive Card/ViewController.swift
+++ Interactive Card/ViewController.swift
@@ -13,7 +13,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @objc func showMiracle() {
+        let slideVC = OverlayView()
+        slideVC.modalPresentationStyle = .custom
+        slideVC.transitioningDelegate = self
+        self.present(slideVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func onButton(_ sender: Any) {
+        showMiracle()
+    }
+}
 
-
+extension ViewController: UIViewControllerTransitioningDelegate {
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        PresentationController(presentedViewController: presented, presenting: presenting)
+    }
 }
 
