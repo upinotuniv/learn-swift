cba6fd1473451ef21ba588cf50ab5a30020b5c1d
LuthfyRa.1204 <Luthfy.Rahmani@mii.co.id>
2023-08-17 15:11:58 +0700

    swift ui test

--- MyNotes/ContentView.swift
+++ MyNotes/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    let notes = ["Notes 1", "Notes 2", "Notes 3"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Image("background-1")
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 25)
+                List(notes, id: \.self) {note in
+                    NavigationLink(
+                        destination: plusButton,
+                        label: {
+                            VStack(alignment: .leading, spacing: 4) {
+                                        Text("Hello World!")
+                                        Text("Hai!")
+                                            .font(.caption)
+                                            .foregroundColor(.gray)
+                                    }
+                                    .padding(.vertical, 16)
+                    })
+                }
+                .listStyle(.inset)
+            }
+            .navigationTitle("My Notes")
+            .navigationBarItems(trailing: plusButton)
         }
-        .padding()
+    }
+
+    
+    var plusButton: some View {
+        Button(action: {
+//            isPresentedNewPost.toggle()
+        }, label: {
+            Image(systemName: "plus")
+        })
     }
 }
 
